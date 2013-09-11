166de5d87c0cc7f0e47dd576ad7a1e19d1576be3
tylercd100 <tylercd100@gmail.com>
2013-09-11 15:43:29 -0600

    added while loop per second

--- serialtest.py
+++ serialtest.py
@@ -2,9 +2,12 @@ import serial
 import time
 
 ser = serial.Serial('ttyUSB0'. 38400, timeout = 1)
+sleeptime = 1
 
-ser.write("01 0D \r")
+while true
+	ser.write("01 0C \r")
 
-speed_hex = ser.readline().split(' ')
-speed = float(int('0x'+speed_hex[3], 0))
-print 'Speed: ', speed, 'km/h'
+	speed_hex = ser.readline().split(' ')
+	speed = float(int('0x'+speed_hex[3], 0))
+	print 'RPM: ', speed, ''
+	time.sleep(sleeptime)
