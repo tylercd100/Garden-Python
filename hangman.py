30fb6dcb479a2a3eabc4148cde56642b0ce80544
Tyler <tyler@tyler-CM1630.(none)>
2013-09-10 20:38:50 -0600

    added the main loop and a function to check the guess

--- hangman.py
+++ hangman.py
@@ -7,3 +7,24 @@ def pick_a_word():
 	return words[word_position]
 
 print(pick_a_word())
+
+def play():
+	word = pick_a_word():
+	while True:
+		guess = get_guess(word)
+		if process_guess(guess, word):
+			print('You win! Well Done!')
+			break
+		if lives_remaining == 0:
+			print('You are Hung!')
+			print('The word was: ' + word)
+			break
+
+def get_guess(word):
+	return 'a'
+
+def process_guess(guess, word):
+	global lives_remaining
+	lives_remaining = lives_remaining - 1
+	return False
+
