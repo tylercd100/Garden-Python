2abae185c7e69f679b524ae82f8480856e32030c
tylercd100 <tylercd100@gmail.com>
2013-09-14 12:48:53 -0600

    3 leds

--- blink.py
+++ blink.py
@@ -2,23 +2,23 @@ from time import sleep
 import RPi.GPIO as GPIO
 GPIO.cleanup()
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
-# GPIO.setup(15, GPIO.OUT)
-# GPIO.setup(16, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
 while 1:
-     GPIO.output(11, False)
+     GPIO.output(13, False)
      sleep(1)
-     GPIO.output(11, True)
+     GPIO.output(13, True)
      sleep(1)
 
-     # GPIO.output(15, False)
-     # sleep(1)
-     # GPIO.output(15, True)
-     # sleep(1)
+     GPIO.output(15, False)
+     sleep(1)
+     GPIO.output(15, True)
+     sleep(1)
 
-     # GPIO.output(16, False)
-     # sleep(1)
-     # GPIO.output(16, True)
-     # sleep(1)
+     GPIO.output(16, False)
+     sleep(1)
+     GPIO.output(16, True)
+     sleep(1)
 
 	 
