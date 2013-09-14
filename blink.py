912b895b94939c3200b96e916c2d9bf809154b54
tylercd100 <tylercd100@gmail.com>
2013-09-14 13:16:51 -0600

    asdf

--- blink.py
+++ blink.py
@@ -1,19 +1,28 @@
 from time import sleep
 import RPi.GPIO as GPIO
+import math
+
 GPIO.cleanup()
 GPIO.setmode(GPIO.BOARD)
+
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(15, GPIO.OUT)
 GPIO.setup(16, GPIO.OUT)
 GPIO.setup(11, GPIO.IN)
 GPIO.setup(12, GPIO.IN)
+
 sleeptime = 0.05
+
 while 1:
+     
      if GPIO.input(11):
           sleeptime = sleeptime + 0.01
+
      if GPIO.input(12):
           sleeptime = sleeptime - 0.01
-          
+     
+     sleeptime = math.fabs(sleeptime)
+
      GPIO.output(13, False)
      sleep(sleeptime)
      GPIO.output(13, True)
