9538f30525cf5ff3fe9fabaafdd485a7e9615d3b
tylercd100 <tylercd100@gmail.com>
2013-09-14 13:14:08 -0600

    asdf

--- blink.py
+++ blink.py
@@ -5,8 +5,15 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(13, GPIO.OUT)
 GPIO.setup(15, GPIO.OUT)
 GPIO.setup(16, GPIO.OUT)
+GPIO.setup(11, GPIO.IN)
+GPIO.setup(12, GPIO.IN)
 sleeptime = 0.05
 while 1:
+     if GPIO.input(11):
+          sleeptime = sleeptime + 0.01
+     if GPIO.input(12):
+          sleeptime = sleeptime - 0.01
+          
      GPIO.output(13, False)
      sleep(sleeptime)
      GPIO.output(13, True)
