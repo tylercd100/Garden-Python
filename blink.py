e558a3e6b28b9eb7a0a4e579cc2d85799f68b1af
tylercd100 <tylercd100@gmail.com>
2013-09-14 12:29:48 -0600

    changed board modeo

--- blink.py
+++ blink.py
@@ -1,24 +1,24 @@
 from time import sleep
 import RPi.GPIO as GPIO
 GPIO.cleanup()
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(13, GPIO.OUT)
-GPIO.setup(15, GPIO.OUT)
-GPIO.setup(16, GPIO.OUT)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(0, GPIO.OUT)
+GPIO.setup(3, GPIO.OUT)
+GPIO.setup(4, GPIO.OUT)
 while 1:
-     GPIO.output(13, False)
+     GPIO.output(0, False)
      sleep(1)
-     GPIO.output(13, True)
-     sleep(1)
-
-     GPIO.output(15, False)
-     sleep(1)
-     GPIO.output(15, True)
+     GPIO.output(0, True)
      sleep(1)
 
-     GPIO.output(16, False)
+     GPIO.output(3, False)
      sleep(1)
-     GPIO.output(16, True)
+     GPIO.output(3, True)
+     sleep(1)
+
+     GPIO.output(4, False)
+     sleep(1)
+     GPIO.output(4, True)
      sleep(1)
 
 	 
