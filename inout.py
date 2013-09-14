8478e91c27f6e70a89b239b32a51e2bcab150654
tylercd100 <tylercd100@gmail.com>
2013-09-14 13:02:33 -0600

    asdf

--- inout.py
+++ inout.py
@@ -6,12 +6,12 @@ GPIO.setup(11, GPIO.IN)
 GPIO.setup(15, GPIO.OUT)
 GPIO.setup(12, GPIO.IN)
 while 1:
-	if GPIO.input(11):
+	if GPIO.input(11) and not GPIO.input(12):
 		GPIO.output(13, False)
 	else:
 		GPIO.output(13, True)
 		
-	if GPIO.input(12):
+	if GPIO.input(12) and not GPIO.input(11):
 		GPIO.output(15, False)
 	else:
 		GPIO.output(15, True)
