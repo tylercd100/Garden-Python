106ca678b4d848be062bdd95074c62db60452eb9
tylercd100 <tylercd100@gmail.com>
2013-10-21 18:38:08 -0600

    removed try catch

--- bookshelfgarden.py
+++ bookshelfgarden.py
@@ -91,15 +91,10 @@ printToFile(logFile,'Success Connecting to arduino')
 
 #mysql
 while True:
-	try:
-		db = MySQLdb.connect(host="127.0.0.1",user="root",passwd="joshua22",db="garden")
-		cur = db.cursor()
-	except _mysql_exceptions.OperationalError:
-		printToFile(logFile,'Error, trying again..')
-		time.sleep(2)
-	else:
-		printToFile(logFile,'Success Connecting to MySQL Database')
-		break
+	db = MySQLdb.connect(host="127.0.0.1",user="root",passwd="joshua22",db="garden")
+	cur = db.cursor()
+	printToFile(logFile,'Success Connecting to MySQL Database')
+	break
 
 	# printToFile(logFile,'Error Connecting to the database')
 
